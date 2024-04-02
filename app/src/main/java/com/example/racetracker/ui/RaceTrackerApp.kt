75b14f4813cbcccb0f7c6444c2111aba8bd03f46
pavlav <pa.lavrentyev@gmail.com>
2024-04-02 21:04:58 +0300

    ver.1 add comments

--- app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
@@ -51,89 +51,89 @@ fun RaceTrackerApp() {
      * использовать с пользовательским объектом сохранения. Но чтобы упростить пример и сосредоточить внимание на
      * сопрограммах, детали реализации которых удалены.
      */
-    val playerOne = remember {
-        RaceParticipant(name = "Player 1", progressIncrement = 1)
+    val playerOne = remember { // игрок1
+        RaceParticipant(name = "Player 1", progressIncrement = 1) //значение по умолчанию 1
     }
-    val playerTwo = remember {
-        RaceParticipant(name = "Player 2", progressIncrement = 2)
+    val playerTwo = remember { // игрок2
+        RaceParticipant(name = "Player 2", progressIncrement = 2) //значение по умолчанию 2 , значит игрок два быстрее на раз
     }
-    var raceInProgress by remember { mutableStateOf(false) }
+    var raceInProgress by remember { mutableStateOf(false) } // прогресс гонки изначально false
 
     if (raceInProgress) {
-        LaunchedEffect(playerOne, playerTwo) {
+        LaunchedEffect(playerOne, playerTwo) {// запуск двух потоков
             coroutineScope {
                 launch { playerOne.run() }
                 launch { playerTwo.run() }
             }
-            raceInProgress = false
+            raceInProgress = false // прогресс гонки false
         }
     }
     RaceTrackerScreen(
-        playerOne = playerOne,
-        playerTwo = playerTwo,
-        isRunning = raceInProgress,
-        onRunStateChange = { raceInProgress = it },
-        modifier = Modifier
+        playerOne = playerOne, // игрок1
+        playerTwo = playerTwo, // игрок2
+        isRunning = raceInProgress, // прогресс
+        onRunStateChange = { raceInProgress = it }, // прогресс
+        modifier = Modifier // модификатор
             .statusBarsPadding()
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
+            .fillMaxSize() // занять максимальный размер контейнера
+            .verticalScroll(rememberScrollState()) // вертикальный скролинг
             .safeDrawingPadding()
-            .padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+            .padding(horizontal = dimensionResource(R.dimen.padding_medium)), // отступ по горизонту
     )
 }
 
 @Composable
-private fun RaceTrackerScreen(
-    playerOne: RaceParticipant,
-    playerTwo: RaceParticipant,
+private fun RaceTrackerScreen( // главный экран UI приложения
+    playerOne: RaceParticipant, // игрок1
+    playerTwo: RaceParticipant, // игрок1
     isRunning: Boolean,
     onRunStateChange: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier // модификатор
 ) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Column( // контейнер список
+        modifier = modifier, // модификатор
+        verticalArrangement = Arrangement.Center, // расположение по вертикали в центре
+        horizontalAlignment = Alignment.CenterHorizontally // расположение по горизонтали в центре
     ) {
-        Text(
-            text = stringResource(R.string.run_a_race),
-            style = MaterialTheme.typography.headlineSmall,
+        Text( // верхний заголовок приложения
+            text = stringResource(R.string.run_a_race), // сам текст взятый из рессурсов string
+            style = MaterialTheme.typography.headlineSmall, // стиль шрифта
         )
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(dimensionResource(R.dimen.padding_medium)),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Column( // контейнер список
+            modifier = Modifier // модификатор
+                .fillMaxSize() // занимать полный размер контейнера
+                .padding(dimensionResource(R.dimen.padding_medium)), // отступ со всех сторон
+            verticalArrangement = Arrangement.Center, // расположение по вертикали в центре
+            horizontalAlignment = Alignment.CenterHorizontally, // расположение по горизонтали в центре
         ) {
-            Icon(
-                painter = painterResource(R.drawable.ic_walk),
-                contentDescription = null,
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+            Icon( // иконка контейнер
+                painter = painterResource(R.drawable.ic_walk), // иконка ресурс
+                contentDescription = null, // описания нету
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)), // отступ со всех сторон
             )
-            StatusIndicator(
-                participantName = playerOne.name,
-                currentProgress = playerOne.currentProgress,
-                maxProgress = stringResource(
+            StatusIndicator( // статус индикатор 1 игрока
+                participantName = playerOne.name, // имя игрока1
+                currentProgress = playerOne.currentProgress, // текущий прогресс
+                maxProgress = stringResource( // максимальный прогресс
                     R.string.progress_percentage,
                     playerOne.maxProgress
                 ),
-                progressFactor = playerOne.progressFactor,
-                modifier = Modifier.fillMaxWidth()
+                progressFactor = playerOne.progressFactor, // значение прогресса вычисленное
+                modifier = Modifier.fillMaxWidth() // модификатор, занять полную ширину контейнера
             )
-            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
-            StatusIndicator(
-                participantName = playerTwo.name,
-                currentProgress = playerTwo.currentProgress,
-                maxProgress = stringResource(
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large))) // пропуск между контейнерами
+            StatusIndicator( // статус индикатор 2 игрока
+                participantName = playerTwo.name, // имя игрока2
+                currentProgress = playerTwo.currentProgress, // текущий прогресс
+                maxProgress = stringResource( // максимальный прогресс
                     R.string.progress_percentage,
                     playerTwo.maxProgress
                 ),
-                progressFactor = playerTwo.progressFactor,
-                modifier = Modifier.fillMaxWidth(),
+                progressFactor = playerTwo.progressFactor, // значение прогресса вычисленное
+                modifier = Modifier.fillMaxWidth(), // модификатор, занять полную ширину контейнера
             )
-            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
-            RaceControls(
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large))) // пропуск между контейнерами
+            RaceControls( // управление программой
                 isRunning = isRunning,
                 onRunStateChange = onRunStateChange,
                 onReset = {
@@ -141,50 +141,50 @@ private fun RaceTrackerScreen(
                     playerTwo.reset()
                     onRunStateChange(false)
                 },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(), // модификатор, занять всю ширину контейнера
             )
         }
     }
 }
 
 @Composable
-private fun StatusIndicator(
-    participantName: String,
-    currentProgress: Int,
-    maxProgress: String,
-    progressFactor: Float,
-    modifier: Modifier = Modifier
+private fun StatusIndicator( // статус индикатор
+    participantName: String, // имя участника стринг
+    currentProgress: Int, // текущий прогресс инт
+    maxProgress: String, // максимальный прокресс стринг
+    progressFactor: Float, // значение прогресса вычисленное
+    modifier: Modifier = Modifier // модификатор
 ) {
-    Row(
-        modifier = modifier
+    Row( // ряд контейнер
+        modifier = modifier // модификатор
     ) {
-        Text(
-            text = participantName,
-            modifier = Modifier.padding(end = dimensionResource(R.dimen.padding_small))
+        Text( // текст
+            text = participantName, // имя участника
+            modifier = Modifier.padding(end = dimensionResource(R.dimen.padding_small)) // отступ справа
         )
-        Column(
-            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        Column( // список контейнер
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)) // вертикальный пробел между контейнерами списка
         ) {
-            LinearProgressIndicator(
-                progress = { progressFactor },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(dimensionResource(R.dimen.progress_indicator_height))
-                    .clip(RoundedCornerShape(dimensionResource(R.dimen.progress_indicator_corner_radius)))
+            LinearProgressIndicator( // линия прогресса
+                progress = { progressFactor }, // значение прогресса
+                modifier = Modifier // модификатор
+                    .fillMaxWidth() // занимать всю ширину контейнера
+                    .height(dimensionResource(R.dimen.progress_indicator_height)) // высота контейнера
+                    .clip(RoundedCornerShape(dimensionResource(R.dimen.progress_indicator_corner_radius))) // обрезание округленным контуром
             )
-            Row(
-                modifier = Modifier.fillMaxWidth(),
+            Row( // ряд контейнер
+                modifier = Modifier.fillMaxWidth(), // модификатор, занять всю ширину контейнера
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    text = stringResource(R.string.progress_percentage, currentProgress),
-                    textAlign = TextAlign.Start,
-                    modifier = Modifier.weight(1f)
+                Text( // текст1 в ряду , (контейнер)
+                    text = stringResource(R.string.progress_percentage, currentProgress), // размер текущего прогресса
+                    textAlign = TextAlign.Start, // текс занимает начало контейнера
+                    modifier = Modifier.weight(1f) // модификатор, вес значения в контейнере ряд
                 )
-                Text(
-                    text = maxProgress,
-                    textAlign = TextAlign.End,
-                    modifier = Modifier.weight(1f)
+                Text( // текс2 в ряду , (контейнер)
+                    text = maxProgress, // значение максимального прогресса
+                    textAlign = TextAlign.End, // текст занимает конец контейнера
+                    modifier = Modifier.weight(1f) // модификатор, вес значения в контейнере ряд
                 )
             }
         }
@@ -192,32 +192,32 @@ private fun StatusIndicator(
 }
 
 @Composable
-private fun RaceControls(
+private fun RaceControls( // управление программой (кнопки)
     onRunStateChange: (Boolean) -> Unit,
-    onReset: () -> Unit,
-    modifier: Modifier = Modifier,
-    isRunning: Boolean = true,
+    onReset: () -> Unit, // сброс
+    modifier: Modifier = Modifier, // модификатор
+    isRunning: Boolean = true, // значение пуска забега, конструктор тру
 ) {
-    Column(
-        modifier = modifier.padding(top = dimensionResource(R.dimen.padding_medium)),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+    Column( // список контейнер
+        modifier = modifier.padding(top = dimensionResource(R.dimen.padding_medium)), // модификатор, отступ сверху
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)) // вертикальное положение, пропуск между контейнерами
     ) {
-        Button(
-            onClick = { onRunStateChange(!isRunning) },
-            modifier = Modifier.fillMaxWidth(),
+        Button( // кнопка контейнер
+            onClick = { onRunStateChange(!isRunning) }, //изначально onRunStateChange = false, а
+            modifier = Modifier.fillMaxWidth(), // модификатор, занять всю ширину контейнера
         ) {
-            Text(if (isRunning) stringResource(R.string.pause) else stringResource(R.string.start))
+            Text(if (isRunning) stringResource(R.string.pause) else stringResource(R.string.start)) // текс кнопки (если пуска забега, то меняется кнопка на паузу, иначе старт)
         }
-        OutlinedButton(
-            onClick = onReset,
-            modifier = Modifier.fillMaxWidth(),
+        OutlinedButton( // выделенная кнопка с контуром
+            onClick = onReset, // сброс при нажатии
+            modifier = Modifier.fillMaxWidth(), // модификатор, занять всю ширину контейнера
         ) {
-            Text(stringResource(R.string.reset))
+            Text(stringResource(R.string.reset)) // текс кнопки
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true) // превьюшка
 @Composable
 fun RaceTrackerAppPreview() {
     RaceTrackerTheme {
